7debd49108cdd0b6cdf076d226ab4745824b5050
alessandrostein <alessandro@alessandrostein.com>
2017-09-30 22:12:44 -0300

    Remove fix code

--- db/migrate/20170930193019_add_category_reference_to_url.rb
+++ db/migrate/20170930193019_add_category_reference_to_url.rb
@@ -1,6 +1,5 @@
 class AddCategoryReferenceToUrl < ActiveRecord::Migration[5.0]
   def change
-    add_reference :urls, :category, foreign_key: true
-    Url.update_all category_id: 1
+    add_reference :urls, :category, foreign_key: true    
   end
 end
